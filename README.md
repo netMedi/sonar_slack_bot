94ebc05bf7e84be597fd6674b585501df6cbba97
Mikhail (Mikko) Timofeev <mikhail.timofeev@elekta.com>
2024-06-12 11:09:01 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# sonar_slack_bot
+
+## Requirements
+
+- bash
+- bun
+- secret_tool (for development only)
+
+## Install
+
+- `secret_tool local # extract secrets`
+- `bun install`
+
+## Run
+
+`bun run bot.ts`
---
+++ bot.ts
@@ -0,0 +1,15 @@
+import { App } from '@slack/bolt';
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
+app.event('app_mention', async ({ event, say }) => {
+  await say(`Hello, <@${event.user}>!`);
+});
+
+(async () => {
+  await app.start();
+  console.log('⚡️ Slack bot is running!');
+})();
