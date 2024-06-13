a5ad593c6197d6526b290881e7f7c20197f04073
Mikhail (Mikko) Timofeev <mikhail.timofeev@elekta.com>
2024-06-13 12:46:48 +0300

    multilple emails

--- src/cli.ts
+++ src/cli.ts
@@ -3,32 +3,33 @@
 import { PatientSearch } from "./patient-search/patient-search";
 
 // Reading command line arguments
-const [email, ...rest] = process.argv.slice(2);
-const explanation = rest.join(" ");
-if (!email || !explanation) {
-  console.error("Please provide an email address and an explanation.");
+const [...emails] = process.argv.slice(2);
+if (!emails || emails.length === 0) {
+  console.error("Please provide an email address or multiple email addresses.");
   process.exit(1);
 }
-if (!email.includes("@")) {
-  console.error("Invalid email address.");
-  process.exit(1);
-}
+for (let email of emails) {
+  if (!email.includes("@")) {
+    console.error("Invalid email address.");
+    email = "";
+  }
 
-if (email !== undefined && explanation !== undefined) {
-  console.log(`Searching for ${email}...`);
+  if (email !== undefined && email !== "") {
+    console.log(`Searching for ${email}...`);
 
-  const patientSearch = new PatientSearch();
-  const results = await patientSearch.search(email); // Jaako, save us
+    const patientSearch = new PatientSearch();
+    const results = await patientSearch.search(email);
 
-  if (results.length === 0) {
-    console.log(`Couldn't find any patient with email address ${email}.`);
-  } else {
-    console.log(
-      `Found ${results.length} patients with email address ${email}:`
-    );
-    results.forEach((result) => {
-      console.log(result);
-    });
+    if (results.length === 0) {
+      console.log(`Couldn't find any patient with email address ${email}.`);
+    } else {
+      console.log(
+        `Found ${results.length} patients with email address ${email}:`
+      );
+      results.forEach((result) => {
+        console.log(result);
+      });
+    }
   }
 }
 
